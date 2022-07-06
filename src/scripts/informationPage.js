e00be867b782b1e5d77193b1fd12b47b0539ed55
Ivannn555w <ivan22cilev@gmail.com>
2022-07-06 20:51:31 +0200

    infromationPage

--- src/scripts/informationPage.js
+++ src/scripts/informationPage.js
@@ -1,10 +1,22 @@
-var x = $(".content");
-var y = $(".information-text");
-$(document).ready(function(){
-  $(".bi").click(function(){
-    x.slideToggle();
-    
-  });
-  
 
-});
+const toTop = document.querySelector(".to-top");
+
+/*window.addEventListener("scroll",()=>{
+  if(window.pageYOffset >100){
+    toTop.classList.add("active");
+  }else{
+    toTop.classList.remove("active");
+  }
+})*/
+
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+      if ($(document).scrollTop() > 100) {
+        toTop.classList.add("active");
+      }
+      else {
+        toTop.classList.remove("active");
+      }
+  });
+});​
