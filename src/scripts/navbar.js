d367272f207b4d59054c7d4cadb83e0427020de7
mkolevska <93546787+mkolevska@users.noreply.github.com>
2022-07-07 20:48:14 +0200

    changes

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -10,15 +10,16 @@ let sidebarById = document.getElementById("#sidebars");
 
 //Profile elements
 let profileElement = document.getElementById("profile");
-console.log(profileElement)
 let welcomeElement = document.getElementById("welcome");
 let nameElement = document.getElementById("nameOfUser");
 let logOutBtn = document.getElementById('log_out');
 logOutBtn.style.visibility = 'hidden';
 
+
 //localStorage Data needed for creating profile
 let name = localStorage.getItem('name');
 let emailForLogIn = localStorage.getItem('email');
+
 
 
 
@@ -45,7 +46,6 @@ if(name != null && name != ''){
   welcomeElement.innerHTML = 'Welcome  : ';
   nameElement.innerHTML = name;
   logOutBtn.style.visibility = 'visible';
-
 }
 
 
@@ -55,7 +55,15 @@ logOutBtn.addEventListener('click', function(){
   nameElement.innerHTML = '';
   localStorage.removeItem('name');
   localStorage.removeItem('email');
+  let localStoragewalletLength = localStorage.getItem("cryptoWalletOfUser");
+  let oneUserCryptoWallet =JSON.parse(localStorage.getItem("cryptoWalletOfUser"));
+  console.log(oneUserCryptoWallet);
+  // let cryptoWalletValueUsers = JSON.parse(localStorage.getItem("cryptoWalletsOfMultipleUsers")) || [];
+  // cryptoWalletValueUsers.push({oneUserCryptoWallet});
+  // localStorage.setItem('cryptoWalletsOfMultipleUsers"',JSON.stringify(cryptoWalletValueUsers));
+  localStorage.removeItem("coinsDb");
   logOutBtn.style.visibility = 'hidden';
+
 })
 
 
@@ -81,7 +89,6 @@ $(document).on('click', '#view_homepage', function() {
   $('#statistics-page').hide();
   $('#simulator-page').hide();
   $('#info-page').hide();
-
   $('#home-page').show();
 });
 $(document).on('click', '#view_statistic', function() {
